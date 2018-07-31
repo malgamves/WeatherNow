85a0a892263e28e20c51817ce7ac9e5b54ed962b
Daniel Phiri <malgamves@gmail.com>
2018-08-01 00:05:25 +0530

    changes

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # WeatherNow
 A simple Weather app using Node.js
 # Dependencies :
-Install Request package using ``` npm install request -S ```
+Install all packages with ``` npm install ```
 
-API used from openweather.org
+[Open Weather API](openweather.org) ws used.
 Sample API call=http://api.openweathermap.org/data/2.5/forecast?id=1253573&APPID=56e2043a628c776ab619d9d393c2b568&units=metric
 
 # How to run ?
-Execute command : ```node app.js``` from command line.
+Execute command : ```node app.js``` from command line. <br>
 Go to your Browser and enter address as ```localhost:8081```
 After successful run, go back to command line and stop the execution using CTRL + C.
--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-var http = require('http');
-var url = 'http://api.openweathermap.org/data/2.5/forecast?id=1253573&APPID=56e2043a628c776ab619d9d393c2b568&units=metric';
+var http = require('http'); // To start the server
 
-var server = http.createServer(function(request,response){
+var url = 'http://api.openweathermap.org/data/2.5/forecast?id=1253573&APPID=56e2043a628c776ab619d9d393c2b568&units=metric'; // API Request URL
+
+var port = 8081; // Port number
+
+// Creating server
+var server = http.createServer(function (request, response) {
   // All logic will go here.
   var request = require('request');
-  request(url,function(err,res,body){
+  request(url, function (err, res, body) {
     var data = JSON.parse(body);
-      response.write("<html><body><div id ='continer'>");
-      response.write("<h1>"+ 'City Name - : ' + data.city['name'] + '<br>' + "</h1>");
-      response.write("<h2>"+ 'Temperature - : ' + data.list[0].main.temp + '<br>' + "</h2>");
-      response.write("<h2>"+ 'Wind Speed - : ' + data.list[0].wind.speed + '<br>' + "</h2>");
-
-      response.write("</div></body></html>");
-      response.end();
+    response.write("<html><body><div id ='container'>");
+    response.write("<h1>" + 'City Name : ' + data.city['name'] + '<br>' + "</h1>");
+    response.write("<h2>" + 'Temperature (Celsius): ' + data.list[0].main.temp + '<br>' + "</h2>");
+    response.write("<h2>" + 'Wind Speed (Km/h) : ' + data.list[0].wind.speed + '<br>' + "</h2>");
+    response.write("<h2>" + 'Summary : ' + data.list[0].weather[0].description + '<br>' + "</h2>");
+    response.write("</div></body></html>");
+    response.end();
   });
 
 
 
 
-}).listen(8081);
+}).listen(port);
+
+console.log('running server on port ' + port);
